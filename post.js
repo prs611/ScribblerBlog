7e0215fc27e46af1fbaf5c46daee51ac53d9da86
Pratyush Vatsal Shukla <pratyush.11702110@gmail.com>
2019-07-13 01:15:40 +0530

    made all the minor changes and the project is ready to be submitted

--- post.js
+++ post.js
@@ -64,8 +64,6 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
-window.onclick = function(event) {
   if (event.target == modal2) {
     modal2.style.display = "none";
   }
